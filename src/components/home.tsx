d257b29e0050823a6b3397333ed4d6a5f1dce384
wojtek2724@gmail.com <wojtek2724@gmail.com>
2025-03-22 20:24:08 +0100

    login and homepage dummies

---
+++ src/components/home.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Home: React.FC = () => {
+  return (
+    <div className="h-screen flex items-center justify-center text-3xl">
+      Welcome to Home Page!
+    </div>
+  );
+};
