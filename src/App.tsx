266d7fa17918481c2bfccca8af4e0dfaf35e085d
wojtek2724@gmail.com <wojtek2724@gmail.com>
2025-03-22 20:24:08 +0100

    login and homepage dummies

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Login } from './components/login.tsx';
+import { Home } from './components/home.tsx';
+import { LoginPanel } from './components/login-panel.tsx';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login/:userType" element={<LoginPanel />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
