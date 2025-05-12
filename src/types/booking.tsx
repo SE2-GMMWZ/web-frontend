9aa8789611517a2a0759a5bf9f513d6be5ef0e84
wojtek2724@gmail.com <wojtek2724@gmail.com>
2025-05-12 10:45:18 +0200

    cringe

---
+++ src/types/booking.tsx
@@ -0,0 +1,12 @@
+export type BookingData = 
+{
+    booking_id: string;
+    dock_id: string;
+    end_date: string;
+    payment_method: string;
+    payment_status: string;
+    people: number;
+    sailor_id: string;
+    start_date: string;
+    user_id: string;
+}
